14676e14a2942bc5d0d90b43099b56471cb59823
Saheer Khan <saheer.khan105@gmail.com>
2024-09-25 22:55:05 +0600

    change

---
+++ bin/assignment_1.dart
@@ -0,0 +1,41 @@
+class Book {
+  static int totalBooks
+
+  String title;
+  String author;
+  int publicationYear;
+  int pagesRead;
+
+  Book(this.title,this.author,this.publicationYear) : pagesRead = 0 {
+  }
+
+ void read(int pages){
+pagesRead += pages;
+  }
+
+ int getPagesRead(){
+    return pagesRead;
+  }
+  String getTitle(){
+    return title;
+}
+
+String getAuthor(){
+    return author;
+}
+
+int getPublicationYear(){
+    return publicationYear;
+}
+
+int getBookAge(){
+    int currentYear = DateTime.now().year;
+    return currentYear - publicationYear;
+}
+
+
+
+
+
+
+}
